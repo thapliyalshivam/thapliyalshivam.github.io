f1facdd72bfbfcd1a9ddad380a692d28c8615eb5
Shivam Thapliyal <thapliyalshivam@gmail.com>
2019-02-28 21:20:12 +0530

    Updates

---
+++ precache-manifest.b6d15e30f3e94892c0bc5669b62f59c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bc7a1890ff021cebd003",
+    "url": "/static/js/main.bc7a1890.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "bc7a1890ff021cebd003",
+    "url": "/static/css/main.426da595.chunk.css"
+  },
+  {
+    "revision": "45196de7998184097323b25f20b6cf38",
+    "url": "/index.html"
+  }
+];
