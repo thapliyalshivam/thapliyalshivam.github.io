fd7433437fdee734826c72d01d3e6e425b37b8e0
Shivam Thapliyal <thapliyalshivam@gmail.com>
2019-12-23 22:29:51 +0530

    feature layout

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -13,19 +13,47 @@ class About extends Component {
 
 
     render() {
-        return (        
-         
-                 <div className="about_wrapper">
-                     <h1>About me</h1>
-                     <img src={require(`../../public/thumbnails/loop.jpeg`)}></img>
-                    <h2>Hi, my name is Shivam Thapliyal. I am a  Graphic Designer/Illustrator based in Bangalore working for Flipkart as a product illustrator. 
+        return (
+
+            <div className="about_wrapper">
+                <h1>About me</h1>
+                <div className="horizontal">
+                    <img src={require(`../../public/thumbnails/about.jpg`)}></img>
+
+                    <p>Hi, my name is Shivam Thapliyal. I am a  Graphic Designer/Illustrator based in Bangalore working for Flipkart as a product illustrator.
                         I am a Computer Science Engineering graduate who after working as a Nodejs developer for 7 months switched to persue Design and Illustrations. My work ranges from simple geometric composition to 3d renderings. From vintage posters to even coding in CSS and Javascript! I also make tech art projects as well.
-                    Wanna say hi?</h2>
-                </div> 
-                
-                
+                    Wanna say hi?</p>
+                </div>
+
+                <div className="horizontal">
+                  <div>
+                   <h3>Featured</h3>
+                    <p>Hi, my name is Shivam Thapliyal. I am a  Graphic Designer/Illustrator based in Bangalore working for Flipkart as a product illustrator.
+                        I am a Computer Science Engineering graduate who after working as a Nodejs developer for 7 months switched to persue Design and Illustrations. My work ranges from simple geometric composition to 3d renderings. From vintage posters to even coding in CSS and Javascript! I also make tech art projects as well.
+                    Wanna say hi?</p>
+                    </div> 
+                    <div>
+                   <h3>Featured</h3>
+                    <p>Hi, my name is Shivam Thapliyal. I am a  Graphic Designer/Illustrator based in Bangalore working for Flipkart as a product illustrator.
+                        I am a Computer Science Engineering graduate who after working as a Nodejs developer for 7 months switched to persue Design and Illustrations. My work ranges from simple geometric composition to 3d renderings. From vintage posters to even coding in CSS and Javascript! I also make tech art projects as well.
+                    Wanna say hi?</p>
+                    </div> 
+                    <div>
+                   <h3>Featured</h3>
+                    <p>Hi, my name is Shivam Thapliyal. I am a  Graphic Designer/Illustrator based in Bangalore working for Flipkart as a product illustrator.
+                        I am a Computer Science Engineering graduate who after working as a Nodejs developer for 7 months switched to persue Design and Illustrations. My work ranges from simple geometric composition to 3d renderings. From vintage posters to even coding in CSS and Javascript! I also make tech art projects as well.
+                    Wanna say hi?</p>
+                    </div> 
+                </div>
+
+
+            </div>
+
+
+
+
         );
-    }2
+    } 2
 
 
 }
