d5d7a25f63ca9515d3ed7194e54331ee18efc0df
Shivam Thapliyal <thapliyalshivam@gmail.com>
2020-08-16 02:55:01 +0530

    Updates

---
+++ precache-manifest.c84a7ea1d6ea6991341c7f2bc08c6e84.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6062e114dce0ceb269ee7acdb4f94d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf88e010c9a74b5b3d29",
+    "url": "/static/css/main.364a7262.chunk.css"
+  },
+  {
+    "revision": "c567fd35e9d7a8af0d7b",
+    "url": "/static/js/2.ffc81a23.chunk.js"
+  },
+  {
+    "revision": "e176efe8519158e0db4328341de2067e",
+    "url": "/static/js/2.ffc81a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf88e010c9a74b5b3d29",
+    "url": "/static/js/main.a202b1d1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "3ea9695ee3af8a5e17413f22249f361a",
+    "url": "/static/media/Futura Heavy font.3ea9695e.ttf"
+  },
+  {
+    "revision": "bddb4b904c6a97fe0e249bb3a568950a",
+    "url": "/static/media/about.bddb4b90.jpg"
+  },
+  {
+    "revision": "d163e8a4640156cb1c0998b5f88b4714",
+    "url": "/static/media/bahoroop.d163e8a4.jpeg"
+  },
+  {
+    "revision": "b4814f1c016f5c06b6fbc0db22fc9c03",
+    "url": "/static/media/cover.b4814f1c.gif"
+  },
+  {
+    "revision": "ecfab62d7831e666f0ff26f9e361a526",
+    "url": "/static/media/cover.ecfab62d.gif"
+  },
+  {
+    "revision": "602e91d4d2849a8018fe990a18373572",
+    "url": "/static/media/cover_1.602e91d4.gif"
+  },
+  {
+    "revision": "3381bcb2e9c9c585088c30fbf081daae",
+    "url": "/static/media/demo_1.3381bcb2.gif"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/static/media/futura medium bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "c013f1594b25b960ac3cf1aacaec93f2",
+    "url": "/static/media/img_1.c013f159.jpeg"
+  },
+  {
+    "revision": "e582b1b39edf06b0ccf242e9c991f29b",
+    "url": "/static/media/img_2.e582b1b3.jpeg"
+  },
+  {
+    "revision": "edc6af0a2de28459868f2d38eff19abe",
+    "url": "/static/media/img_3.edc6af0a.jpeg"
+  },
+  {
+    "revision": "c97aa4d0ecaaa656908626165cc117a3",
+    "url": "/static/media/img_4.c97aa4d0.jpeg"
+  },
+  {
+    "revision": "d846f8a5402c145ac938b4533fc80bee",
+    "url": "/static/media/img_5.d846f8a5.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a95afe9b8a03a11332775f14771acafa",
+    "url": "/static/media/loop.a95afe9b.jpeg"
+  },
+  {
+    "revision": "be287873bcce24d0ac0e899c3fe497dc",
+    "url": "/static/media/loop_1.be287873.gif"
+  },
+  {
+    "revision": "81ab87bc88d0a161b069e4e16a41546e",
+    "url": "/static/media/loop_2.81ab87bc.gif"
+  },
+  {
+    "revision": "a3f939bfd18ecd4a6c44da1747838ef0",
+    "url": "/static/media/loop_3.a3f939bf.gif"
+  },
+  {
+    "revision": "6abae0378f987d8832c02b5edbf21d8a",
+    "url": "/static/media/loop_4.6abae037.gif"
+  },
+  {
+    "revision": "d728a3dba7deb0990ca1dfefed200c82",
+    "url": "/static/media/proxima_Bold.d728a3db.ttf"
+  },
+  {
+    "revision": "feface72008e6857da27f939e1eceffa",
+    "url": "/static/media/proxima_Regular.feface72.ttf"
+  },
+  {
+    "revision": "7bd0a4c62609f996460b4db12000eb5c",
+    "url": "/static/media/proxima_Semibold.7bd0a4c6.ttf"
+  },
+  {
+    "revision": "532aaef1988c0cb07d3cb2ff93a47127",
+    "url": "/static/media/proxima_Thin.532aaef1.ttf"
+  },
+  {
+    "revision": "936aac231cc9494f74ec12660497c341",
+    "url": "/static/media/setup_1.936aac23.png"
+  },
+  {
+    "revision": "8f6b85839a03f36cb027f4e07886794e",
+    "url": "/static/media/show_1.8f6b8583.png"
+  }
+]);
